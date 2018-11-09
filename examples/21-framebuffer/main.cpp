84df79151569fb8919013adad83f2d98d947ce58
yunyuncc <wyy584322202@outlook.com>
2018-11-09 17:02:05 +0800

    refactor

--- examples/21-framebuffer/main.cpp
+++ examples/21-framebuffer/main.cpp
@@ -1,4 +1,5 @@
 #include "camera.hpp"
+#include "opengl_app.hpp"
 #include "shader.hpp"
 #include "util.hpp"
 #include <GLFW/glfw3.h>
@@ -13,74 +14,6 @@
 #include <vector>
 
 using namespace std;
-const size_t screen_width = 1280;
-const size_t screen_height = 720;
-// resize window callback
-void framebuffer_size_callback(GLFWwindow *, int width, int height) {
-  // update user view
-  glViewport(0, 0, width, height);
-}
-
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
-
-myopengl::camera &get_camera() {
-  static glm::vec3 camera_pos(0.f, 0.f, 3.f);
-  static glm::vec3 camera_up(0.f, 1.f, 0.f);
-  static myopengl::camera::config cfg{.yaw = -90.f,
-                                      .pitch = 0.f,
-                                      .speed = 2.5f,
-                                      .sensitivity = 0.1f,
-                                      .zoom = 45.f};
-  static myopengl::camera c(camera_pos, camera_up, cfg);
-  return c;
-}
-float deltaTime = 0.0f; // 当前帧与上一帧的时间差
-float lastFrame = 0.0f; // 上一帧的时间
-
-float lastX = 400, lastY = 300;
-
-bool firstMouse = true;
-void mouse_callback(GLFWwindow *, double xpos, double ypos) {
-
-  if (firstMouse) // 这个bool变量初始时是设定为true的
-  {
-    lastX = xpos;
-    lastY = ypos;
-    firstMouse = false;
-  }
-
-  float xoffset = xpos - lastX;
-  float yoffset =
-      -1 *
-      (ypos - lastY); // 注意这里是相反的，因为y坐标是从底部往顶部依次增大的
-  lastX = xpos;
-  lastY = ypos;
-  get_camera().deal_mouse_move(xoffset, yoffset);
-}
-
-void scroll_callback(GLFWwindow *, double /* xoffset*/, double yoffset) {
-  get_camera().deal_mouse_scroll(yoffset);
-}
-
-// deal esc
-void processInput(GLFWwindow *window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, true);
-
-  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-    get_camera().deal_keyboard(myopengl::camera::move::forward, deltaTime);
-  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-    get_camera().deal_keyboard(myopengl::camera::move::back, deltaTime);
-  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-    get_camera().deal_keyboard(myopengl::camera::move::left, deltaTime);
-  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-    get_camera().deal_keyboard(myopengl::camera::move::right, deltaTime);
-  if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
-    cout << "pos:" << myopengl::to_string(get_camera().get_pos())
-         << " yaw:" << get_camera().get_yaw()
-         << " pitch:" << get_camera().get_pitch()
-         << "  front:" << myopengl::to_string(get_camera().get_front()) << endl;
-}
 // three vertices of a triangle in Normalized Device Coordinates
 vector<float> cubeVertices = {
     // positions          // texture Coords
@@ -169,52 +102,6 @@ vector<unsigned int> setup_buffer() {
   return {cubeVAO, planeVAO, quadVAO};
 }
 
-void update_projection(const myopengl::shader &shader_) {
-  glm::mat4 projection(1.0f);
-  projection = glm::perspective(
-      glm::radians(get_camera().get_zoom()),
-      screen_width / static_cast<float>(screen_height), 0.1f, 100.0f);
-
-  shader_.set_uniform("projection", projection);
-}
-void update_camera_view(const myopengl::shader &shader_) {
-  glm::mat4 view = get_camera().get_view();
-  shader_.set_uniform("view", view);
-}
-
-GLFWwindow *init() {
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  // create window
-  GLFWwindow *window = glfwCreateWindow(screen_width, screen_height,
-                                        "LearnOpenGL", nullptr, nullptr);
-  if (window == nullptr) {
-    std::cout << "Failed to create GLFW window" << std::endl;
-    glfwTerminate();
-  }
-  glfwMakeContextCurrent(window);
-
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-  glfwSetCursorPosCallback(window, mouse_callback);
-  glfwSetScrollCallback(window, scroll_callback);
-  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-  // init glad
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::cout << "Failed to initialize GLAD" << std::endl;
-    glfwTerminate();
-  }
-  glEnable(GL_DEPTH_TEST);
-  glDepthFunc(GL_LESS);
-  glEnable(GL_BLEND);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  // init user view
-  glViewport(0, 0, screen_width, screen_height);
-  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-  return window;
-}
-
 void set_floor_model_and_draw(const myopengl::shader &light_shader_) {
   light_shader_.set_uniform("model", glm::mat4(1.0f));
   glDrawArrays(GL_TRIANGLES, 0, 6);
@@ -271,92 +158,52 @@ unsigned int load_texture(const std::string &img_path, GLenum texture_unit) {
   }
   return textureID;
 }
-tuple<unsigned int, unsigned int, unsigned int> setup_framebuffer() {
-  unsigned int framebuffer;
-  glGenFramebuffers(1, &framebuffer);
-  glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
 
-  // 生成纹理
-  unsigned int texColorBuffer;
-  glGenTextures(1, &texColorBuffer);
-  glBindTexture(GL_TEXTURE_2D, texColorBuffer);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, screen_width, screen_height, 0, GL_RGB,
-               GL_UNSIGNED_BYTE, nullptr);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glBindTexture(GL_TEXTURE_2D, 0);
+using namespace myopengl;
 
-  // 将它附加到当前绑定的帧缓冲对象
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
-                         texColorBuffer, 0);
-
-  unsigned int rbo;
-  glGenRenderbuffers(1, &rbo);
-  glBindRenderbuffer(GL_RENDERBUFFER, rbo);
-  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, screen_width,
-                        screen_height);
-  glBindRenderbuffer(GL_RENDERBUFFER, 0);
-
-  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT,
-                            GL_RENDERBUFFER, rbo);
-
-  if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-    std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!"
-              << std::endl;
-    throw std::runtime_error("framebuffer");
+class myapp : public opengl_app {
+public:
+  explicit myapp(const std::string &screen_fs_path) {
+    shader_ =
+        std::make_shared<shader>("../examples/21-framebuffer/depth_testing.vs",
+                                 "../examples/21-framebuffer/depth_testing.fs");
+    screen_shader_ = std::make_shared<shader>(
+        "../examples/21-framebuffer/screen.vs", screen_fs_path);
   }
-
-  glBindFramebuffer(GL_FRAMEBUFFER, 0);
-  return {framebuffer, texColorBuffer, rbo};
-}
-int main(int argc, char **argv) {
-  if(argc != 2){
-  	cout << "usage :" << argv[0] << " screen.fs" << endl;
-	return 0;
+  void before_render() override {
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    VAOs_ = setup_buffer();
+    cubeTexture_ = load_texture("../img/container.jpg", GL_TEXTURE0);
+    floorTexture_ = load_texture("../img/metal.png", GL_TEXTURE0);
+    shader_->use();
+    shader_->set_uniform("texture1", 0 /*use texture unit 0*/);
+    screen_shader_->use();
+    screen_shader_->set_uniform("screenTexture", 0);
+    auto [framebuffer, tex_color_buffer, renderbuffer] = setup_framebuffer();
+    framebuffer_ = framebuffer;
+    tex_color_buffer_ = tex_color_buffer;
+    renderbuffer_ = renderbuffer;
   }
-  std::string screen_fs_path(argv[1]);
-  auto window = init();
-  auto VAOs = setup_buffer();
-  myopengl::shader shader_("../examples/21-framebuffer/depth_testing.vs",
-                           "../examples/21-framebuffer/depth_testing.fs");
-  myopengl::shader screen_shader("../examples/21-framebuffer/screen.vs",
-                                 screen_fs_path);
-  // render loop
-
-  auto cubeTexture = load_texture("../img/container.jpg", GL_TEXTURE0);
-  auto floorTexture = load_texture("../img/metal.png", GL_TEXTURE0);
-  shader_.use();
-  shader_.set_uniform("texture1", 0 /*use texture unit 0*/);
-  screen_shader.use();
-  screen_shader.set_uniform("screenTexture", 0);
-  auto [framebuffer, tex_color_buffer, renderbuffer] = setup_framebuffer();
-  while (!glfwWindowShouldClose(window)) {
-    float currentFrame = glfwGetTime();
-    deltaTime = currentFrame - lastFrame;
-    lastFrame = currentFrame;
-
-    processInput(window);
-
+  void render() override {
     // bind to framebuffer and draw scene as we normally would to color texture
-    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer_);
     glEnable(GL_DEPTH_TEST);
 
-    // do render
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    shader_.use();
-    update_camera_view(shader_);
-    update_projection(shader_);
+    shader_->use();
+    update_camera_view(*shader_);
+    update_projection(*shader_);
 
-    glBindTexture(GL_TEXTURE_2D, cubeTexture);
-    glBindVertexArray(VAOs[0]);
-    set_cube_model_and_draw(shader_, 1.0f);
-
+    glBindTexture(GL_TEXTURE_2D, cubeTexture_);
+    glBindVertexArray(VAOs_[0]);
+    set_cube_model_and_draw(*shader_, 1.0f);
     // draw floar
-    shader_.use();
-    glBindTexture(GL_TEXTURE_2D, floorTexture);
-    glBindVertexArray(VAOs[1]);
-    set_floor_model_and_draw(shader_);
+    shader_->use();
+    glBindTexture(GL_TEXTURE_2D, floorTexture_);
+    glBindVertexArray(VAOs_[1]);
+    set_floor_model_and_draw(*shader_);
 
     // now bind back to default framebuffer and draw a quad plane with the
     // attached framebuffer color texture
@@ -370,17 +217,71 @@ int main(int argc, char **argv) {
                // we won't be able to see behind the quad anyways)
     glClear(GL_COLOR_BUFFER_BIT);
 
-    screen_shader.use();
-    glBindVertexArray(VAOs[2]);
+    screen_shader_->use();
+    glBindVertexArray(VAOs_[2]);
     glBindTexture(GL_TEXTURE_2D,
-                  tex_color_buffer); // use the color attachment texture as the
-                                     // texture of the quad plane
+                  tex_color_buffer_); // use the color attachment texture as the
+                                      // texture of the quad plane
     glDrawArrays(GL_TRIANGLES, 0, 6);
-
-    // double buffer
-    glfwSwapBuffers(window);
-    glfwPollEvents();
   }
-  glfwTerminate();
+
+  tuple<unsigned int, unsigned int, unsigned int> setup_framebuffer() {
+    unsigned int framebuffer;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+    // 生成纹理
+    unsigned int texColorBuffer;
+    glGenTextures(1, &texColorBuffer);
+    glBindTexture(GL_TEXTURE_2D, texColorBuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, screen_width, screen_height, 0,
+                 GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    // 将它附加到当前绑定的帧缓冲对象
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
+                           texColorBuffer, 0);
+
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, screen_width,
+                          screen_height);
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT,
+                              GL_RENDERBUFFER, rbo);
+
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+      std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!"
+                << std::endl;
+      throw std::runtime_error("framebuffer");
+    }
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    return {framebuffer, texColorBuffer, rbo};
+  }
+
+private:
+  std::shared_ptr<shader> shader_;
+  std::shared_ptr<shader> screen_shader_;
+  std::vector<unsigned int> VAOs_;
+  unsigned int cubeTexture_ = -1;
+  unsigned int floorTexture_ = -1;
+  unsigned int framebuffer_;
+  unsigned int tex_color_buffer_;
+  unsigned int renderbuffer_;
+};
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    cout << "usage :" << argv[0] << " screen.fs" << endl;
+    return 0;
+  }
+  std::string screen_fs_path(argv[1]);
+  myapp app(screen_fs_path);
+  app.run();
   return 0;
 }
